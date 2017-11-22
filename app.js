855aee32b20cf1ac4e30697c95820c30e47cf8e7
rupertgh <33883153+rupertgh@users.noreply.github.com>
2017-11-22 15:06:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -63,5 +63,5 @@ app.get('/', customersweb.listall);
 app.use(app.router);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port') + app.local.apiurl);
 });
